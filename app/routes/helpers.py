78c4fe3ec9e7e135e9019a18afd171042f025dd6
P Marino <peter.c.marino@gmail.com>
2026-02-21 15:36:13 -0500

    Revisions to helpers

--- app/routes/helpers.py
+++ app/routes/helpers.py
@@ -8,11 +8,22 @@ def compute_marked_metrics(marked_scenarios):
     Returns a list of dicts:
         {marked, pi, pc, volatility, velocity, event_count}
     """
+    if not marked_scenarios:
+        return []
+
+    # Batch-load all ScenarioEvent rows in one query instead of one per scenario
+    marked_ids = [m.id for m in marked_scenarios]
+    all_links = ScenarioEvent.query.filter(
+        ScenarioEvent.marked_scenario_id.in_(marked_ids)
+    ).all()
+
+    links_by_id = {}
+    for link in all_links:
+        links_by_id.setdefault(link.marked_scenario_id, []).append(link)
+
     results = []
     for marked in marked_scenarios:
-        event_links = ScenarioEvent.query.filter_by(
-            marked_scenario_id=marked.id
-        ).all()
+        event_links = links_by_id.get(marked.id, [])
 
         events_data = [
             (link.event_code, float(link.weight), link.linked_at)
